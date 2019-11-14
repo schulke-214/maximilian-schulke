280dc66f86deb075742f1350acb87beb2134c681
schulke-214 <schulkemaximilian@gmail.com>
2019-11-14 21:29:09 +0100

    feat: add an example component

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled('button')`
+	padding: 1rem;
+	outline: none;
+	border: none;
+	background-color: hotpink;
+	color: white;
+	cursor: pointer;
+
+	&:hover {
+		background-color: salmon;
+	}
+`;
