607b725b6feeea032379b32f45eca755e747ab1f
schulke-214 <schulkemaximilian@gmail.com>
2019-11-16 19:25:53 +0100

    build: add gatsby plugins for browser and offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
+	siteMetadata: {
+		title: `Maximilian Schulke`,
+		description: `A place where i post about nerd stuff with a lot of technical details.`,
+		author: `@schulke-214`
+	},
 	plugins: [
 		{
 			resolve: `gatsby-plugin-alias-imports`,
@@ -23,9 +28,23 @@ module.exports = {
 			options: {
 				repositoryName: 'maximilian-schulke',
 				accessToken: process.env.PRISMIC_API_KEY,
-				path: '/preview',
-				previews: true
+				defaultLang: 'en-us',
+				previews: false
 			}
-		}
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `My Blog`,
+				short_name: `My Blog`,
+				start_url: `/`,
+				background_color: `#000000`,
+				theme_color: `#ffffff`,
+				display: `standalone`,
+				icon: `static/assets/favicon.png`
+			}
+		},
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-react-helmet`
 	]
 };
