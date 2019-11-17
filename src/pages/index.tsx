a649c173dc8cca7997ec5b1d93ef41a5087790f2
schulke-214 <schulkemaximilian@gmail.com>
2019-11-17 15:33:44 +0100

    refactor: wrap homepage in a dedicated container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,32 @@
 import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
 import { Layout } from 'layouts/default';
-import { RichText, asText } from 'components/core/RichText';
+import { RichText } from 'components/core/RichText';
 import SEO from 'components/layout/SEO';
 
 interface HomePageProps {
 	data: any;
 }
 
+const HomePageContainer = styled.div`
+	h1 {
+		margin-top: 0;
+	}
+`;
+
 const HomePage: FunctionComponent<HomePageProps> = ({ data }) => {
 	const document = data.prismic.allHomePages.edges[0].node;
 
 	return (
 		<Layout>
-			<SEO lang='en' title='test' />
+			<HomePageContainer>
+				<SEO lang='en' title='test' />
 
-			<RichText render={document.title} />
-			<RichText render={document.introduction} />
+				<RichText render={document.title} />
+				<RichText render={document.introduction} />
+			</HomePageContainer>
 		</Layout>
 	);
 };
