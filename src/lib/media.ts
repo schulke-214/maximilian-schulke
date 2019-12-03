46906047072fc9cbf9054de2bd8545b2cf54c199
schulke-214 <hello@maximilianschulke.com>
2019-12-03 23:12:57 +0100

    feat: add a new media target

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -2,4 +2,5 @@ import { rem } from 'polished';
 
 export const desktop = `@media (min-width: ${rem(992)})`;
 export const tablet = `@media (max-width: ${rem(992)})`;
+export const landscape = `@media (max-width: ${rem(760)})`;
 export const mobile = `@media (max-width: ${rem(576)})`;
