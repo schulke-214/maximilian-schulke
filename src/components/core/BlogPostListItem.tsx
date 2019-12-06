9a130ccdb794360bb00fbcb5edac0f5783abca07
schulke-214 <schulkemaximilian@gmail.com>
2019-12-06 21:48:22 +0100

    feat: enhance the blogpostitem

--- src/components/core/BlogPostListItem.tsx
+++ src/components/core/BlogPostListItem.tsx
@@ -1,8 +1,7 @@
 import React, { FunctionComponent } from 'react';
-import styled, { css } from 'styled-components';
-import { rem, transparentize } from 'polished';
+import styled from 'styled-components';
+import { rem } from 'lib/polished';
 
-import { mobile } from 'lib/media';
 import { asText } from 'components/core/RichText';
 import { Link } from 'gatsby';
 
@@ -10,20 +9,79 @@ interface BlogPostListItemProps {
 	post: {
 		title: any;
 		description: any;
+		image?: {
+			teaser: {
+				dimensions: {
+					width: number;
+					height: number;
+				};
+				url: string;
+			};
+		};
 		_meta: {
 			uid: string;
 		};
 	};
+	className?: string;
 }
 
-const BlogPostListItem: FunctionComponent<BlogPostListItemProps> = ({ post }) => (
-	<div style={{ marginBottom: 40 }}>
-		<h2>{asText(post.title)}</h2>
-		<p>{asText(post.description)}</p>
-		<Link to={`/post/${post._meta.uid}`}>Read Article</Link>
-	</div>
-);
+const BlogPostListItemContentContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	.image {
+		img {
+			display: block;
+			width: 100%;
+		}
+	}
+
+	.content {
+		padding-right: 0;
+
+		p {
+			margin-bottom: ${props => rem(props.theme.spacings.medium)};
+		}
+	}
+`;
+
+const BlogPostListItem: FunctionComponent<BlogPostListItemProps> = ({ post, className }) => {
+	const postHref = `/post/${post._meta.uid}`;
+
+	return (
+		<div className={className}>
+			<BlogPostListItemContentContainer>
+				{post.image ? (
+					<p className='image mobile-fullscreen-image'>
+						<Link to={postHref}>
+							<img src={post.image.teaser.url} />
+						</Link>
+					</p>
+				) : null}
+				<div className='content'>
+					<Link to={postHref}>
+						<h2>{asText(post.title)}</h2>
+					</Link>
+					<p>{asText(post.description)}</p>
+				</div>
+			</BlogPostListItemContentContainer>
+			<div>
+				<Link to={postHref}>Read Article</Link>
+			</div>
+		</div>
+	);
+};
 
 export default styled(BlogPostListItem)<BlogPostListItemProps>`
-	color: red;
+	display: flex;
+	flex-direction: column;
+	margin-top: ${props => rem(props.theme.spacings.xlarge)};
+
+	h2 {
+		margin-top: 0;
+	}
+
+	&:first-child {
+		margin-top: 0;
+	}
 `;
