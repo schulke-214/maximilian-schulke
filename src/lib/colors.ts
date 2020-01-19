0d795119b5a267f1ebf96e73efa2bb72e77175c3
schulke-214 <schulkemaximilian@gmail.com>
2020-01-19 17:51:15 +0100

    feat: add new colors

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -2,9 +2,11 @@ export const colors = {
 	black: '#131415',
 	white: '#fff',
 	pink: '#ff1493',
-	gray: '#dedede',
+	// gray: '#dedede',
+	gray: '#f0f0f0',
 	red: '#E74C3C',
 	violet: '#5e31dc',
+	blue: '#3155dc',
 
 	// added later
 	green: '#39cc9f',
