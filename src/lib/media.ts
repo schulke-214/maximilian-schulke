f0a2ae8b3c969a49676ee5fc1a976378903b5911
schulke-214 <schulkemaximilian@gmail.com>
2020-02-02 00:17:42 +0100

    feat: export breakpoints as constants

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -1,6 +1,12 @@
 import { rem } from 'lib/polished';
 
-export const desktop = `@media (min-width: ${rem(992)})`;
-export const tablet = `@media (max-width: ${rem(992)})`;
-export const landscape = `@media (max-width: ${rem(760)})`;
-export const mobile = `@media (max-width: ${rem(576)})`;
+export const breakpoints = {
+    lg: 992,
+    md: 760,
+    sm: 576
+};
+
+export const desktop = `@media (min-width: ${rem(breakpoints.lg)})`;
+export const tablet = `@media (max-width: ${rem(breakpoints.lg)})`;
+export const landscape = `@media (max-width: ${rem(breakpoints.md)})`;
+export const mobile = `@media (max-width: ${rem(breakpoints.sm)})`;
