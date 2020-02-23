98dd14fc4bafdaf2fc2a788f729d704684319036
schulke-214 <schulkemaximilian@gmail.com>
2020-02-23 15:44:42 +0100

    feat(share): make the sahre section customizable

--- src/components/core/Share.tsx
+++ src/components/core/Share.tsx
@@ -1,44 +1,72 @@
 import React, { FunctionComponent } from 'react';
+import { useStaticQuery } from 'gatsby';
 
 import Button from 'components/ui/Button';
 
 import { rem } from 'lib/polished';
 import { landscape } from 'lib/media';
+import { graphql } from 'gatsby';
+import { asText } from 'components/core/RichText';
 
-const Share: FunctionComponent<{}> = () => (
-	<div
-		css={`
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: center;
-			margin: ${(props: any) => rem(props.theme.spacings.large)} 0;
-
-			h2,
-			p {
-				margin-top: 0;
-			}
-
-			p {
-				margin-bottom: ${(props: any) => rem(props.theme.spacings.small)};
-				max-width: 50%;
-				text-align: center;
-
-				${landscape} {
-					max-width: 100%;
+const Share: FunctionComponent<{}> = () => {
+	const data = useStaticQuery(graphql`
+		{
+			prismic {
+				allSharePosts(lang: "en-us") {
+					edges {
+						node {
+							cta
+							ctaAction {
+								... on PRISMIC__ExternalLink {
+									url
+								}
+							}
+							info
+							title
+						}
+					}
 				}
 			}
+		}
+	`);
 
-			${Button} {
-				margin-bottom: 0;
-			}
-		`}>
-		<h2>Enjoying this Post?</h2>
-		<p>If you appreciate my content, think about sharing my posts or giving this project a star on github! </p>
-		<a href='https://github.com/schulke-214/maximilian-schulke' target='__blank'>
-			<Button>Go To GitHub</Button>
-		</a>
-	</div>
-);
+	const doc = data.prismic.allSharePosts.edges[0].node;
+
+	return (
+		<div
+			css={`
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				margin: ${(props: any) => rem(props.theme.spacings.large)} 0;
+
+				h2,
+				p {
+					margin-top: 0;
+				}
+
+				p {
+					margin-bottom: ${(props: any) => rem(props.theme.spacings.medium)};
+					max-width: 50%;
+					text-align: center;
+
+					${landscape} {
+						max-width: 100%;
+					}
+				}
+
+				${Button} {
+					margin-bottom: 0;
+				}
+			`}>
+			<h2>{asText(doc.title)}</h2>
+			<p>{asText(doc.info)}</p>
+			<a href={doc.ctaAction.url} target='__blank'>
+				<Button>{doc.cta}</Button>
+			</a>
+		</div>
+	);
+};
 
 export default Share;
