02e58fbf9f84981cb69a2c7e1de3fa3b9aad953e
schulke-214 <schulkemaximilian@gmail.com>
2020-02-23 15:45:04 +0100

    feat(layout): include a footer

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -8,7 +8,8 @@ import { ThemeType } from 'lib/themes';
 
 import Header from 'components/layout/Header';
 import Container from 'components/layout/Container';
-import CookieConsent from '../components/layout/CookieConsent';
+import CookieConsent from 'components/layout/CookieConsent';
+import Footer from 'components/layout/Footer';
 
 const initializeTheme = (): ThemeType => (Cookies.get('theme') as ThemeType) || ThemeType.Light;
 
@@ -40,6 +41,7 @@ export const Layout: FunctionComponent<{ location: any }> = ({ children, locatio
 			<GlobalStyles />
 			<Header toggleTheme={toggleTheme} location={location} />
 			<Container>{children}</Container>
+			<Footer />
 			<CookieConsent />
 		</ThemeProvider>
 	);
