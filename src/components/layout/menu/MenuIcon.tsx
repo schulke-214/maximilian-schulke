349c5b0f487a813dd1d4ebfb24ab9779ed65630d
schulke-214 <schulkemaximilian@gmail.com>
2020-02-24 21:38:06 +0100

    refactor: clean up code for the menu icon and overlay

--- src/components/layout/menu/MenuIcon.tsx
+++ src/components/layout/menu/MenuIcon.tsx
@@ -1,65 +1,68 @@
 import React, { FunctionComponent } from 'react';
 import styled, { css } from 'styled-components';
 
-import { rem } from "lib/polished";
+import { rem } from 'lib/polished';
 
-import { Invertible, Openable } from "components/layout/Header";
+import { Invertible, Openable } from 'components/layout/Header';
 
 interface MenuIconProps extends Invertible, Openable {
-    onClick(): void;
-    className?: string;
+	onClick(): void;
+	className?: string;
 }
 
 const MenuIconContainer = styled.button<MenuIconProps>`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    background-color: transparent;
-    cursor: pointer;
-    outline: none;
-    position: relative;
-    width: ${props => rem((props.theme.spacings.small * 2) + 24)};
-    height: ${props => rem((props.theme.spacings.small * 2) + 16)};
-    padding: ${props => rem(props.theme.spacings.small)};
-    right: -${props => rem(props.theme.spacings.small)};
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	background-color: transparent;
+	cursor: pointer;
+	outline: none;
+	position: relative;
+	width: ${props => rem(props.theme.spacings.small * 2 + 24)};
+	height: ${props => rem(props.theme.spacings.small * 2 + 16)};
+	padding: ${props => rem(props.theme.spacings.small)};
 
-    span {
-        opacity: 1;
-        display: block;
-        width: 1.5rem;
-        height: ${rem(2)};
-        background-color: ${props => props.theme.colors.foreground};
-        transition: all ${props => props.theme.animation.duration.instant}s;
-        
-        ${props => (props.inverted || props.open) && css`
-            background-color: ${props.theme.colors.highlightForeground};
-        `}
-    }
-    
-    ${props => props.open && css`
-        z-index: 99;
-    
-        span:first-child {
-            transform: rotate(45deg) translate(5px, 5px);
-        }
-        
-        span:nth-child(2) {
-            opacity: 0;
-        }
-        
-        span:last-child {
-            transform: rotate(-45deg) translate(5px, -5px);
-        }
-    `}
+	span {
+		opacity: 1;
+		display: block;
+		width: 1.5rem;
+		height: ${rem(2)};
+		background-color: ${props => props.theme.colors.foreground};
+		transition: all ${props => props.theme.animation.duration.instant}s;
+
+		${props =>
+			(props.inverted || props.open) &&
+			css`
+				background-color: ${props.theme.colors.highlightForeground};
+			`}
+	}
+
+	${props =>
+		props.open &&
+		css`
+			z-index: ${props => props.theme.layers.overlay.content};
+
+			span:first-child {
+				transform: rotate(45deg) translate(5px, 5px);
+			}
+
+			span:nth-child(2) {
+				opacity: 0;
+			}
+
+			span:last-child {
+				transform: rotate(-45deg) translate(5px, -5px);
+			}
+		`}
 `;
 
 const MenuIcon: FunctionComponent<MenuIconProps> = ({ className, inverted, open, onClick }) => (
-    <MenuIconContainer open={open} onClick={onClick} className={className} inverted={inverted}>
-        <span/>
-        <span/>
-        <span/>
-    </MenuIconContainer>
+	<MenuIconContainer open={open} onClick={onClick} className={className} inverted={inverted}>
+		<span />
+		<span />
+		<span />
+	</MenuIconContainer>
 );
 
 export default MenuIcon;
--- src/components/layout/menu/MenuOverlay.tsx
+++ src/components/layout/menu/MenuOverlay.tsx
@@ -1,41 +1,39 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
-import { linearGradient } from "lib/polished";
+import { linearGradient } from 'lib/polished';
 
 interface MenuOverlayProps {
-    className?: string;
+	className?: string;
 }
 
 const MenuOverlayContainer = styled.div`
-    display: flex;
-    align-items: center;
-    background-color: ${props => linearGradient(props.theme.colors.highlightGradient)};
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    z-index: 98;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    color: ${props => props.theme.colors.highlightForeground};
+	display: flex;
+	align-items: center;
+	background-color: ${props => linearGradient(props.theme.colors.highlightGradient)};
+	width: 100vw;
+	height: 100vh;
+	position: fixed;
+	z-index: ${props => props.theme.layers.overlay.background};
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	color: ${props => props.theme.colors.highlightForeground};
 `;
 
 const MenuOverlayContent = styled.div`
-    height: min-content;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center; 
+	height: min-content;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `;
 
 const MenuOverlay: FunctionComponent<MenuOverlayProps> = ({ className, children }) => (
-    <MenuOverlayContainer className={className}>
-        <MenuOverlayContent>
-            { children }
-        </MenuOverlayContent>
-    </MenuOverlayContainer>
+	<MenuOverlayContainer className={className}>
+		<MenuOverlayContent>{children}</MenuOverlayContent>
+	</MenuOverlayContainer>
 );
 
 export default MenuOverlay;
