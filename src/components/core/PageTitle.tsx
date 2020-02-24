6a0c21248e025718fab716f11e6409e30e24a9cf
schulke-214 <schulkemaximilian@gmail.com>
2020-02-24 12:24:39 +0100

    feat: add page components

---
+++ src/components/core/PageTitle.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+import { RichText } from 'components/core/RichText';
+import { rem } from 'lib/polished';
+
+const RawPageTitle: FunctionComponent<{ title: any; className?: string }> = ({ title, className }) => (
+	<div className={className}>
+		<RichText render={title} />
+	</div>
+);
+
+const PageTitle = styled(RawPageTitle)`
+	margin-bottom: ${props => rem(props.theme.spacings.large)};
+
+	&,
+	h1,
+	span {
+		text-align: center;
+	}
+
+	h1 {
+		margin-bottom: ${props => rem(props.theme.spacings.small)};
+	}
+
+	span {
+		font-size: ${rem(12)};
+	}
+`;
+
+export default PageTitle;
---
+++ src/templates/page.tsx
@@ -0,0 +1,73 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { graphql } from 'gatsby';
+
+import { Layout } from 'layouts/default';
+import { asText } from 'components/core/RichText';
+
+import SEO from 'components/layout/SEO';
+
+import TextSlice from 'components/slices/Text';
+import ImageSlice from 'components/slices/Image';
+import CodeSlice from 'components/slices/Code';
+import PageTitle from 'components/core/PageTitle';
+
+import { rem } from 'lib/polished';
+
+interface PageProps {
+	data: any;
+	location: any;
+}
+
+const PageSlices: FunctionComponent<{ slices: any }> = ({ slices }) => {
+	return slices.map((slice: any, index: number) => {
+		switch (slice.type) {
+			case 'text':
+				return <TextSlice key={index} slice={slice} />;
+
+			case 'image':
+				return <ImageSlice key={index} slice={slice} />;
+
+			case 'code':
+				return <CodeSlice key={index} slice={slice} />;
+
+			default:
+				return <></>;
+		}
+	});
+};
+
+const Page: FunctionComponent<PageProps> = ({ data, location }) => {
+	if (!data.prismic.page.edges[0]) {
+		return <p>404</p>;
+	}
+
+	const page = data.prismic.page.edges[0].node;
+
+	return (
+		<Layout location={location}>
+			<SEO lang='en' title={asText(page.title)} />
+			<PageTitle title={page.title} />
+		</Layout>
+	);
+};
+
+export const query = graphql`
+	query PageQuery($uid: String!) {
+		prismic {
+			page: allPages(uid: $uid) {
+				edges {
+					node {
+						_meta {
+							id
+							uid
+						}
+						title
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default Page;
