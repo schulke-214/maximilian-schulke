261f78bc29d042dc32d3261ccccbf13b77905f5e
schulke-214 <schulkemaximilian@gmail.com>
2020-02-24 23:10:09 +0100

    feat: init form components

---
+++ src/components/ui/form/Select.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+const SelectContainer = styled.select``;
+
+const Select: FunctionComponent<{}> = () => {
+	return (
+		<SelectContainer>
+			<span>e</span>
+		</SelectContainer>
+	);
+};
+
+export default Select;
