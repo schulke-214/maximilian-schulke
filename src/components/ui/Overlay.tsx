da9c2c0d30e2423fe4473fba72c9082374cfda39
schulke-214 <schulkemaximilian@gmail.com>
2020-05-09 23:55:12 +0200

    fix: update the overlay component

--- src/components/ui/Overlay.tsx
+++ src/components/ui/Overlay.tsx
@@ -1,12 +1,7 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import styled from 'styled-components';
-import { linearGradient } from 'lib/polished';
+import { rem } from 'lib/polished';
 
-/**
- * 
- * 	display: flex;
-	align-items: center;
- */
 
 const RawOverlay: FunctionComponent<{ className?: string }> = ({ className, children }) => {
 	useEffect(() => {
@@ -23,16 +18,19 @@ const RawOverlay: FunctionComponent<{ className?: string }> = ({ className, chil
 };
 
 const Overlay = styled(RawOverlay)`
+	display: flex;
+	flex-direction: column;
 	position: fixed;
 	z-index: ${props => props.theme.layers.overlay.background};
+	padding: ${props => rem(props.theme.spacings.large * 2)} ${props => rem(props.theme.spacings.large)};
 	width: 100vw;
 	height: 100vh;
 	top: 0;
 	left: 0;
 	right: 0;
 	bottom: 0;
-	background-color: ${props => linearGradient(props.theme.colors.highlightGradient)};
-	color: ${props => props.theme.colors.highlightForeground};
+	background-color: ${props => props.theme.colors.navigationBackground};
+	color: ${props => props.theme.colors.navigationForeground};
 `;
 
 export default Overlay;
