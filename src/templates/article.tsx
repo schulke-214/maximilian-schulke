e6d50a8214ea7df380f18cf9faf41f2373ecf058
schulke-214 <schulkemaximilian@gmail.com>
2020-05-11 22:43:24 +0200

    feat: enhance the article seo tags

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,17 +1,52 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import Layout from 'layouts/default';
+import SEO from 'components/layout/SEO';
 
 interface ArticleProps {
 	data: any;
 }
 
 const Article: FunctionComponent<ArticleProps> = ({ data }) => {
+	const tags: string[] = [];
+	const { publicURL: image } = data.article.banner || {};
+
+	const meta: object[] = [
+		{
+			property: 'og:type',
+			content: 'article'
+		},
+		{
+			property: 'article:published_time',
+			content: data.article.published
+		},
+		{
+			property: 'article:section',
+			content: 'article'
+		},
+		...tags.map(tag => ({
+			property: 'article:tag',
+			content: tag
+		}))
+	];
+
+	if (data.article.modified)
+		meta.push({
+			property: 'article:modified_time',
+			content: data.article.modified
+		});
+
 	return (
 		<Layout readMode={true}>
+			<SEO
+				title={data.article.title}
+				description={data.article.excerpt}
+				image={ image || ''}
+				meta={meta}
+			/>
+			<h1>{data.article.title}</h1>
 			<MDXRenderer>{data.article.body}</MDXRenderer>
 		</Layout>
 	);
@@ -24,6 +59,12 @@ export const query = graphql`
 			title
 			body
 			timeToRead
+			excerpt
+			published
+			modified
+			banner {
+				publicURL
+			}
 		}
 	}
 `;
