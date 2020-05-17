fd9cb62c46188ed81e9085dcdb7f225e26e3dad4
schulke-214 <schulkemaximilian@gmail.com>
2020-05-17 03:30:20 +0200

    feat: add a category link to the article list item

--- src/components/core/ArticleListItem.tsx
+++ src/components/core/ArticleListItem.tsx
@@ -1,23 +1,36 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
 
 import { rem } from 'lib/polished';
+import { Link } from 'gatsby';
 
 interface ArticleListItemProps {
 	className?: string;
 	title: string;
 	published: string;
 	slug: string;
+	category: {
+		name: string;
+		slug: string;
+		color: string;
+	};
+	withCategory: boolean;
 	timeToRead: number;
 }
 
-const ArticleListItem: FunctionComponent<ArticleListItemProps> = ({ className, title, published, slug, timeToRead }) => (
-	<Link className={className} to={slug} key={slug} css={`display: block; margin-bottom: ${(props: any) => rem(props.theme.spacings.medium)};`}>
-		<h3 css={`margin: 0;`}>{title}</h3>
-		<code>// {published} • {timeToRead} min</code>
-	</Link>
+const ArticleListItem: FunctionComponent<ArticleListItemProps> = ({ className, title, published, slug, category, withCategory, timeToRead }) => (
+	<li className={className} key={slug}>
+		<Link to={slug}><h3>{title}</h3></Link>
+		<code>-- {published} • {timeToRead} min{withCategory ? <> • <Link to={category.slug}>{category.name}</Link></> : null }</code>
+	</li>
 );
 
 
-export default styled(ArticleListItem)<ArticleListItemProps>``;
+export default styled(ArticleListItem)<ArticleListItemProps>`
+	display: block;
+	margin-bottom: ${(props: any) => rem(props.theme.spacings.medium)};
+
+	h3 {
+		margin: 0;
+	}
+`;
