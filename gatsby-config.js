ecb1e806998ed111e37176be6696055a71971e92
schulke-214 <hello@maximilianschulke.com>
2020-08-22 12:51:10 +0200

    update the gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,14 +82,6 @@ const seo = [
 			icon: 'static/assets/favicon.png',
 		},
 	},
-	{
-		resolve: 'gatsby-plugin-google-analytics',
-		options: {
-			trackingId: 'UA-165957143-1',
-			anonymize: true,
-			respectDNT: true
-		}
-	},
 	'gatsby-plugin-offline',
 	'gatsby-plugin-react-helmet',
 ];
@@ -107,32 +99,32 @@ module.exports = {
 		categories: [
 			{
 				name: 'Frontend',
-				slug: '/frontend',
+				slug: '/category/frontend',
 				color: '#62d0dd',
 			},
 			{
 				name: 'Backend',
-				slug: '/backend',
+				slug: '/category/backend',
 				color: '#ff8e21',
 			},
 			{
 				name: 'Compiler',
-				slug: '/compiler',
+				slug: '/category/compiler',
 				color: '#a2d39b',
 			},
 			{
 				name: 'Infrastructure',
-				slug: '/infrastructure',
+				slug: '/category/infrastructure',
 				color: '#ceb4ff',
 			},
 			{
 				name: 'Security',
-				slug: '/security',
+				slug: '/category/security',
 				color: '#a0b7ff',
 			},
 			{
 				name: 'Linux',
-				slug: '/linux',
+				slug: '/category/linux',
 				color: '#ff708b',
 			},
 		],
@@ -141,7 +133,33 @@ module.exports = {
 			cta: 'Support me on Patreon!',
 			url: 'https://www.patreon.com/maximilianschulke',
 			description: 'To get access to alot of patron only content, choose the next topics i write about and much more.'
-		}
+		},
+		footer: {
+			legal: [
+				{
+					slug: '/legal-notice',
+					title: 'Legal Notice'
+				},
+				{
+					slug: '/data-privacy',
+					title: 'Data Privacy'
+				}
+			],
+			social: [
+				{
+					href: 'https://github.com/schulke-214',
+					title: 'GitHub'
+				},
+				{
+					href: 'https://reddit.com/u/schulke-214',
+					title: 'Reddit'
+				},
+				{
+					href: 'mailto:hello@maximilianschulke.com',
+					title: 'Mail'
+				}
+			],
+		},
 	},
 	plugins: [
 		...dev,
